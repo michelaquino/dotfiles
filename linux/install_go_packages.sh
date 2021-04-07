8a4ec45052fffbb530c7523a258a7f55bfadeb36
Michel Aquino <michel.aquino@g.globo>
2021-04-07 10:20:41 -0300

    Add go tools

--- linux/install_go_packages.sh
+++ linux/install_go_packages.sh
@@ -5,6 +5,7 @@ echo "VS Code to Golang tools"
 echo "----------------"
 export GO111MODULE=on
 
+go get -v honnef.co/go/tools/cmd/staticcheck
 go get -v github.com/ramya-rao-a/go-outline
 go get -v github.com/go-delve/delve/cmd/dlv
 go get -u -v golang.org/x/tools/cmd/godoc
