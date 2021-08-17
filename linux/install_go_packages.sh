ef603526f200b0a867195c6870314df9cd26d087
michel.aquino <michel.aquino@g.globo>
2021-08-17 11:01:05 -0300

    Change go install dependencies

--- linux/install_go_packages.sh
+++ linux/install_go_packages.sh
@@ -5,27 +5,27 @@ echo "VS Code to Golang tools"
 echo "----------------"
 export GO111MODULE=on
 
-go get -v honnef.co/go/tools/cmd/staticcheck
-go get -v github.com/ramya-rao-a/go-outline
-go get -v github.com/go-delve/delve/cmd/dlv
-go get -u -v golang.org/x/tools/cmd/godoc
-go get -u -v github.com/derekparker/delve/cmd/dlv
-go get -u -v github.com/nsf/gocode
-go get -u -v github.com/rogpeppe/godef
-go get -u -v github.com/golang/lint/golint
-go get -u -v github.com/lukehoban/go-outline
-go get -u -v sourcegraph.com/sqs/goreturns
-go get -u -v golang.org/x/tools/cmd/gorename
-go get -u -v github.com/tpng/gopkgs
-go get -u -v github.com/newhook/go-symbols
-go get -u -v golang.org/x/tools/cmd/guru
-go get -u -v golang.org/x/tools/cmd/goimports
-go get -u -v github.com/motemen/gore/cmd/gore
-go get -u -v github.com/stamblerre/gocode
-go get -u -v github.com/cweill/gotests/...
-go get -u -v github.com/fatih/gomodifytags
-go get -u -v golang.org/x/tools/gopls@latest
-go get -u -v github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+go install -v honnef.co/go/tools/cmd/staticcheck
+go install -v github.com/ramya-rao-a/go-outline
+go install -v github.com/go-delve/delve/cmd/dlv
+go install -v golang.org/x/tools/cmd/godoc
+go install -v github.com/derekparker/delve/cmd/dlv
+go install -v github.com/nsf/gocode
+go install -v github.com/rogpeppe/godef
+go install -v github.com/golang/lint/golint
+go install -v github.com/lukehoban/go-outline
+go install -v sourcegraph.com/sqs/goreturns
+go install -v golang.org/x/tools/cmd/gorename
+go install -v github.com/tpng/gopkgs
+go install -v github.com/newhook/go-symbols
+go install -v golang.org/x/tools/cmd/guru
+go install -v golang.org/x/tools/cmd/goimports
+go install -v github.com/motemen/gore/cmd/gore
+go install -v github.com/stamblerre/gocode
+go install -v github.com/cweill/gotests/...
+go install -v github.com/fatih/gomodifytags
+go install -v golang.org/x/tools/gopls@latest
+go install -v github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 
 echo "----------------"
 echo "Installing Gitleaks"
